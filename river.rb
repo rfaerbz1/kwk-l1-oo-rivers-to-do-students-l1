283b45e0138669a689791ac12e624385e6f9f79d
rfaerbz1 <bfaerber@cox.net>
2018-06-07 23:03:30 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -19,35 +19,35 @@ end
 nile = River.new 
 nile.name = "Nile"
 nile.length = 6853
-nile.:countries = "Egypt", "Sudan", "South Sudan", "Ethiopia", "Uganda", "Congo", "Kenya", "Tanzania", "Rwanda", "Burundi", "Eritrea"
+nile.countries = "Egypt", "Sudan", "South Sudan", "Ethiopia", "Uganda", "Congo", "Kenya", "Tanzania", "Rwanda", "Burundi", "Eritrea"
 nile.discharge = 2830
 
 mississippi = River.new 
 mississippi.name = "Mississippi"
 mississippi.length = 2320
-mississippi.:countries = "USA"
+mississippi.countries = "USA"
 mississippi.discharge = 593000
 
 amazon = River.new 
 amazon.name = "Amazon"
 amazon.length = 6992.6 
-amazon.:countries = "Brazil", "Columbia", "Peru"
+amazon.countries = "Brazil", "Columbia", "Peru"
 amazon.discharge = 209000
 
 seine = River.new 
 seine.name = "Seine"
 seine.length = 777
-seine.:countries = "France"
+seine.countries = "France"
 seine.discharge = 560
 
 yangtze = River.new
 yangtze.name = "Yangtze"
 yangtze.length = 6357
-yangtze.:countries = "China"
+yangtze.countries = "China"
 yangtze.discharge = 30166
 
 euphrates = River.new
 euphrates.name = "Euphrates"
 euphrates.length = 2800
-euphrates.:countries = "Iraq", "Syria", "Turkey"
+euphrates.countries = "Iraq", "Syria", "Turkey"
 euphrates.discharge = 356
