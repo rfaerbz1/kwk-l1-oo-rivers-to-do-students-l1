46a1600b60aa44f9ed9fdb4a74c4c95f1370d6e9
rfaerbz1 <bfaerber@cox.net>
2018-06-07 22:48:21 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -17,13 +17,19 @@ class River
 end 
 
 nile = River.new 
+nile.name = "Nile"
 
 mississippi = River.new 
+nile.name = "Mississippi"
 
 amazon = River.new 
+amazon.name = "Amazon"
 
 seine = River.new 
+seine.name = "Seine"
 
 yangtze = River.new
+yangtze.name = "Yangtze"
 
 euphrates = River.new
+euphrates.name = "Euphrates"
