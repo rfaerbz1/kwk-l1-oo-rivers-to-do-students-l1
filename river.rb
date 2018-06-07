48858b85516f87158031b61d5f08886b0f14fad6
rfaerbz1 <bfaerber@cox.net>
2018-06-07 22:39:48 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,6 +1,13 @@
 # river.rb
 class River 
   def initialize(name, length, :countries, discharge)
-    
+    @name = name
+    @length = length
+    @countries = :countries
+    @discharge = discharge
   end
+  
+  def flood 
+    
+  end 
 end 
